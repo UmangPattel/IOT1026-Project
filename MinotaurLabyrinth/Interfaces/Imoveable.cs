dd4da849adf541545a4da1160a681182642a693f
UmangPattel <133813528+UmangPattel@users.noreply.github.com>
2023-06-24 18:17:20 +0000

    Immovable.cs

---
+++ MinotaurLabyrinth/Interfaces/Imoveable.cs
@@ -0,0 +1,10 @@
+namespace MinotaurLabyrinth
+{
+    public interface IMoveable
+    {
+        bool DisplaySense(Hero hero, int heroDistance);
+        void Activate(Hero hero, Map map);
+
+        void Move(Hero hero, Map map);
+    }
+}
