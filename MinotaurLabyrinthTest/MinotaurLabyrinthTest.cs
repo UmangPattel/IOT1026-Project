712b4abd41e30275ba39eff59a103a589de6ad03
UmangPattel <133813528+UmangPattel@users.noreply.github.com>
2023-06-24 13:27:02 -0400

    Update MinotaurLabyrinthTest.cs
    2nd updated version of Assignment test.cs

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -2,9 +2,11 @@
 
 namespace MinotaurLabyrinthTest
 {
+    // The class that will have all the properties about the hero in A6
     [TestClass]
     public class Tests
     {
+        //Same goes with the Method
         [TestMethod]
         public void DummyTest()
         {
