910b0b27bb09000865885428b99c8884d0a77e41
UmangPattel <133813528+UmangPattel@users.noreply.github.com>
2023-06-24 18:17:56 +0000

    MinotaurLabyrinth Method

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -2,15 +2,33 @@
 
 namespace MinotaurLabyrinthTest
 {
-    // The class that will have all the properties about the hero in A6
     [TestClass]
-    public class Tests
+    public class RoomTests
     {
-        //Same goes with the Method
         [TestMethod]
-        public void DummyTest()
+
+        public void GelMoveTest()
         {
-            Assert.AreNotSame(1, 2);
+            Hero hero = new Hero();
+            var gelLocation = new Location(1, 1);
+            Charizard gel = new(gelLocation);
+            Map map = new Map(4, 4);
+            map.GetRoomAtLocation(gelLocation).AddMonster(gel);
+            map.SetRoomAtLocation(new Location(2, 1), RoomType.Pit);
+
+
+
+
+            gel.Move(hero, map);
+            //New location should be (1,1)
+            var expectedLocation = new Location(2, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
+            gel.Move(hero, map);
+            expectedLocation = new Location(3, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
+            gel.Move(hero, map);
+            expectedLocation = new Location(3, 1);
+            Assert.AreEqual(expectedLocation, gel.GetLocation());
         }
     }
 }
